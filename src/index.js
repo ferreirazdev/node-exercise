6e1bc1b144aa34791ceb71a9132729da28c21eea
Flávio Ferreira <business.flavioferreira@gmail.com>
2021-05-08 22:53:49 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -42,6 +42,50 @@ app.get('/projects', (request, response) => {
   return response.json(results);
 });
 
+app.post('/projects', (request, response) => {
+  const { title, owner } = request.body;
+
+  const project = { id:uuid(), title, owner };
+
+  projects.push(project);
+
+  return response.json(project);
+});
+
+app.put('/project/:id', (request, response) => {
+  const { id } = request.params;
+  const { title, owner } = request.body;
+
+  const projectIndex = projects.findIndex(project => projects.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found '});
+  }
+
+  const project = {
+    id,
+    title,
+    owner
+  };
+
+  projects[projectIndex] = project;
+
+  return response.json(project);
+});
+
+app.delete('/projects/:id', (request, response) => {
+  const { id } = request.params;
+
+  const projectIndex = project.findIndex(project => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found' });
+  }
+
+  projects.splice(projectIndex, 1);
+  
+  return response.status(204).send();
+})
 
 
 app.listen(3333, () => {
