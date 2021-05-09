b7d0416eea16b486ffe3b343bcda3087b39ab2a7
Flávio Ferreira <business.flavioferreira@gmail.com>
2021-05-08 22:45:21 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,49 @@
 const express = require('express');
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
+app.use(express.json());
+
+const projects = [];
+
+function logRequests(request, response, next) {
+  const { method, url } = request;
+  
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.time(logLabel);
+
+  next();
+
+  console.timeEnd(logLabel);
+}
+
+function validateProjectId(request, response, next) {
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: 'Invalide project ID' })
+  }
+
+  return next();
+}
+
+app.use(logRequests);
+app.use('/projects/:id', validateProjectId);
+
+app.get('/projects', (request, response) => {
+  const { title } = request.query;
+
+  const results = title
+  ? projects.filter(project => project.title.includes(title))
+  : projects;
+
+  return response.json(results);
+});
+
+
+
 app.listen(3333, () => {
-  console.log('Back-end iniciado!')
-})
+  console.log('Back-end iniciado!');
+});
